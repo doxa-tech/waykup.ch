d0e5e4d29b9f793b32db2d52a5cfa415c8ae9fad
hausmanne <54555072+hausmanne@users.noreply.github.com>
2020-05-04 09:20:29 +0200

    js file added

---
+++ assets/javascripts/application.js
@@ -0,0 +1,21 @@
+function stickyNav() {
+    window.onscroll = function() {myFunction()};
+      
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+    
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+}
+function googleAnalytics() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-159516284-1');
+}
